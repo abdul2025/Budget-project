044462cbad00aa9f8d094896034e3a84e325c900
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2021-10-26 12:57:01 +0300

    update icons

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ let UIController = (() => {
                 <div class="right clearfix">
                     <div class="item__value">${formatNumber(obj.value, type)}</div>
                     <div class="item__delete">
-                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        <button class="item__delete--btn"><ion-icon name="close-circle-outline"></ion-icon></button>
                     </div>
                 </div>
             </div>`;

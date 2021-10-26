c2a22159f4fdfb6ed6dab05cd0aaa64dd5e1cdfb
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2021-10-26 13:00:20 +0300

    update appending Icons

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ let UIController = (() => {
                     <div class="item__value">${formatNumber(obj.value, type)}</div>
                     <div class="item__percentage">21%</div>
                     <div class="item__delete">
-                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                        <button class="item__delete--btn"><ion-icon name="close-circle-outline"></ion-icon></button>
                     </div>
                 </div>
             </div>`;
